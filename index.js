cef3d9e55cd06473c1e5c0e2c5f3ca94277f7474
Malek Mammar <malek.mammar@voxity.fr>
2015-08-18 03:30:49 +0200

    adding event handlers

--- index.js
+++ index.js
@@ -27,205 +27,283 @@
  *
  */
 
-var base_url = 'https://api.voxity.fr'; 
+// var base_url = 'https://api.voxity.fr'; 
+var base_url = 'http://localhost:3000'; 
 
+var access_token;
 var gh = (function() {
-  'use strict';
+    'use strict';
 
-  var access_token;
 
-  var tokenFetcher = (function() {
-    var clientID = 'ch2NtN3S25ImoYHaSDsr';
-    var redirectUri = chrome.identity.getRedirectURL();
-    var redirectRe = new RegExp(redirectUri + '[#\?](.*)');
-    access_token = null;
+    var tokenFetcher = (function() {
+        var clientID = 'ch2NtN3S25ImoYHaSDsr';
+        var redirectUri = chrome.identity.getRedirectURL();
+        var redirectRe = new RegExp(redirectUri + '[#\?](.*)');
+        access_token = null;
+
+        return {
+            getToken: function(interactive, callback) {
+                // In case we already have an access_token cached, simply return it.
+                var options;
+
+                chrome.storage.sync.get({
+                  access_token: null,
+                }, function(items) {
+                    access_token = items.access_token;
+                    if(access_token) return callback(null, access_token);
+                    
+                    options = {
+                        'interactive': interactive,
+                        url: base_url + '/api/v1/dialog/authorize?client_id=' + clientID +
+                            '&response_type=token' +
+                            '&redirect_uri=' + redirectUri
+                    };
+                        
+                    chrome.identity.launchWebAuthFlow(options, function(responseUri) {
+                        if (chrome.runtime.lastError) return callback(new Error(chrome.runtime.lastError))
+                        // Upon success the response is appended to redirectUri, e.g.
+                        // https://{app_id}.chromiumapp.org/provider_cb#access_token={value}&refresh_token={value}
+                        // or:
+                        // https://{app_id}.chromiumapp.org/provider_cb#code={value}
+                        var matches = responseUri.match(redirectRe);
+                        if (matches && matches.length > 1)
+                            handleProviderResponse(parseRedirectFragment(matches[1]));
+                        else
+                            callback(new Error('Invalid redirect URI'));
+                    });
+                });
+
+                function parseRedirectFragment(fragment) {
+                    var pairs = fragment.split(/&/);
+                    var values = {};
+
+                    pairs.forEach(function(pair) {
+                        var nameval = pair.split(/=/);
+                        values[nameval[0]] = nameval[1];
+                    });
+
+                    return values;
+                }
+
+                function handleProviderResponse(values) {
+                    if (values.hasOwnProperty('access_token'))
+                        setAccessToken(values.access_token);
+                    else
+                        callback(new Error('No access_token available.'));
+                }
+
+                function setAccessToken(token) {
+                    access_token = token;
+                    chrome.storage.sync.set({
+                        access_token: token
+                    }, function() {
+                        callback(null, access_token);
+                    });
+                }
+            },
+
+            removeCachedToken: function(token_to_remove) {
+                if (access_token === token_to_remove) {
+                  chrome.storage.sync.remove("access_token"); //when the token is expired we must delete it from the storage in goal to ask a new one
+                  access_token = null;
+                }
+            }
+        };
+    })();
+
+    function xhrWithAuth(method, url, params, interactive, callback) {
+        var retry = true;
+        getToken();
+
+        function getToken() {
+            tokenFetcher.getToken(interactive, function(error, token) {
+            if (error) return callback(error);
+
+            access_token = token;
+            requestStart();
+          });
+        }
+
+        function requestStart() {
+            var xhr = new XMLHttpRequest();
+            var requestBody = params;
+            xhr.open(method, url);
+            xhr.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+            xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
+            xhr.onload = requestComplete;
+            xhr.send(requestBody);
+        }
+
+        function requestComplete() {
+            console.log(this);
+            // if ( && this.response === "Unauthorized")
+            if (this.status == 401 && retry) {
+                retry = false;
+                tokenFetcher.removeCachedToken(access_token);
+                access_token = null;
+                getToken();
+            } else if (this.status == 429) {
+                console.log("429 code error");
+                //notify();
+            } else {
+                callback(null, this.status, this.response);
+            }
+        }
+    }
+
+    function interactiveSignIn() {
+        tokenFetcher.getToken(true, function(error, access_token) {});
+    }
 
     return {
-      getToken: function(interactive, callback) {
-        // In case we already have an access_token cached, simply return it.
-        var options;
+        tokenFetcher: tokenFetcher,
+        signIn: function () {
+          interactiveSignIn();
+        },
+        makeCall: function (exten) {
+            var message = {
+                action: base_url + '/api/v1/channel',
+                method: "POST",
+                parameters: JSON.stringify({'exten': exten})
+            };
+            function onCallSuccess(val, status, response) {
+                response = JSON.parse(response);
+                var title = null;
+                var message = null;
 
-        chrome.storage.sync.get({
-          access_token: null,
-        }, function(items) {
-          access_token = items.access_token;
-          options = {
-            'interactive': interactive,
-            url: base_url + '/api/v1/dialog/authorize?client_id=' + clientID +
-                '&response_type=token' +
-                '&redirect_uri=' + redirectUri
-          };
-          if(access_token) {
-            callback(null, access_token);
-            return;
-          }
-          chrome.identity.launchWebAuthFlow(options, function(responseUri) {
-            if (chrome.runtime.lastError) {
-              callback(new Error(chrome.runtime.lastError));
-              return;
+                if(response.status === 1){
+                  title = 'Demande validée';
+                  message = 'Votre téléphone va sonner d\'ici quelques instants.';
+                }else{
+                  title = response.data.title;
+                  message = response.data.message;
+                }
+
+                var notification_vars = {
+                  type: 'basic', 
+                  iconUrl: 'icon128.png',
+                  title: title, 
+                  message: message 
+                }
+
+                chrome.notifications.getAll(function(notifications){
+                    if(notifications.clicktocall){
+                        chrome.notifications.clear('clicktocall', function(e){});
+                    }
+                });
+
+                chrome.notifications.create('clicktocall', notification_vars, function(n) {});
             }
-            // Upon success the response is appended to redirectUri, e.g.
-            // https://{app_id}.chromiumapp.org/provider_cb#access_token={value}
-            //     &refresh_token={value}
-            // or:
-            // https://{app_id}.chromiumapp.org/provider_cb#code={value}
-            var matches = responseUri.match(redirectRe);
-            if (matches && matches.length > 1)
-              handleProviderResponse(parseRedirectFragment(matches[1]));
-            else
-              callback(new Error('Invalid redirect URI'));
-          });
-        });
-
-        function parseRedirectFragment(fragment) {
-          var pairs = fragment.split(/&/);
-          var values = {};
-
-          pairs.forEach(function(pair) {
-            var nameval = pair.split(/=/);
-            values[nameval[0]] = nameval[1];
-          });
-
-          return values;
+            xhrWithAuth(message.method, message.action, message.parameters, true, onCallSuccess);
         }
-
-        function handleProviderResponse(values) {
-          if (values.hasOwnProperty('access_token')) {
-            setAccessToken(values.access_token);
-          } else {
-            callback(new Error('No access_token available.'));
-          }
-        }
-
-        function setAccessToken(token) {
-          access_token = token;
-          chrome.storage.sync.set({
-            access_token: token
-          }, function() {
-            callback(null, access_token);
-          });
-        }
-      },
-
-      removeCachedToken: function(token_to_remove) {
-        if (access_token === token_to_remove) {
-          chrome.storage.sync.remove("access_token"); //when the token is expired we must delete it from the storage in goal to ask a new one
-          access_token = null;
-        }
-      }
-    };
-  })();
-
-  function xhrWithAuth(method, url, params, interactive, callback) {
-    var retry = true;
-    getToken();
-
-    function getToken() {
-      tokenFetcher.getToken(interactive, function(error, token) {
-        if (error) {
-          callback(error);
-          return;
-        }
-        access_token = token;
-        requestStart();
-      });
-    }
-
-    function requestStart() {
-      var xhr = new XMLHttpRequest();
-      var requestBody = params;
-      xhr.open(method, url);
-      xhr.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
-      xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
-      xhr.onload = requestComplete;
-      xhr.send(requestBody);
-    }
-
-    function requestComplete() {
-      // if ( && this.response === "Unauthorized")
-      if (this.status == 401 && retry) {
-        retry = false;
-        tokenFetcher.removeCachedToken(access_token);
-        access_token = null;
-        getToken();
-      } else {
-        callback(null, this.status, this.response);
-      }
-    }
-  }
-
-  function interactiveSignIn() {
-    tokenFetcher.getToken(true, function(error, access_token) {
-
-    });
-  }
-
-  return {
-    signIn: function () {
-      interactiveSignIn();
-    },
-    makeCall: function (exten) {
-      var message = {
-        action: base_url + '/api/v1/channel',
-        method: "POST",
-        parameters: JSON.stringify({'exten': exten})
       };
-      function onCallSuccess(val, status, response) {
-        response = JSON.parse(response);
-        var title = null;
-        var message = null;
-
-        if(response.status === 1){
-          title = 'Demande validée';
-          message = 'Votre téléphone va sonner d\'ici quelques instants.';
-        }else{
-          title = response.data.title;
-          message = response.data.message;
-        }
-
-        var notification_vars = {
-          type: 'basic', 
-          iconUrl: 'icon128.png',
-          title: title, 
-          message: message 
-          }
-
-        chrome.notifications.getAll(function(notifications){
-          if(notifications.clicktocall){
-            chrome.notifications.clear('clicktocall', function(e){});
-          }
-        })
-
-        chrome.notifications.create(
-          'clicktocall',
-          notification_vars,
-          function(n) {} );
-      }
-      xhrWithAuth(message.method, message.action, message.parameters, true, onCallSuccess);
-    }
-  };
 })();
 
+// Adding context item
 chrome.runtime.onInstalled.addListener(function() {
-  var context = "selection";
-  var title = "Appeler le numéro <%s>";
-  var id = chrome.contextMenus.create({"title": title, "contexts":[context],
-                                         "id": "context" + context});  
+    var context = "selection";
+    var title = "Appeler le numéro <%s>";
+    var id = chrome.contextMenus.create({"title": title, "contexts":[context], "id": "context" + context});  
 });
-
 chrome.contextMenus.onClicked.addListener(onClickHandler);
 
 function onClickHandler(info, tab) {
-  try{
-    if (info.selectionText) {
-      gh.makeCall(info.selectionText);
+    try{
+        if (info.selectionText) {
+            gh.makeCall(info.selectionText);
+        }
+    } catch(ex){
+        console.log(ex);
+        gh.signIn();
     }
-  } catch(ex){
-    console.log(ex);
-    gh.signIn();
-  }
 };
 
+// Redirection listener
 chrome.browserAction.onClicked.addListener(function (tab) { //Fired when User Clicks ICON
-  chrome.tabs.update({url: "https://client.voxity.fr"});
+    // chrome.tabs.update({url: "https://client.voxity.fr"});
+    notify('ringing', "un appel .. un appel")
 });
+
+// callto: click listener for sellsy integration
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    if(sender.id !== chrome.runtime.id) return;
+    gh.makeCall(message.exten);
+  
+    console.log(message)  
+    console.log(sender)
+    console.log(sendResponse)
+})
+
+// Events listener
+var socket = io.connect('http://localhost:3000/', {
+    query:"access_token=acmSFwLNfuT6REzttvoY5A8SO7cDcwiZ0UKv872wPJFEHpGZ9H4dukwNsPJJuyWNZvf6cYc0F1zkoGk0baIOHpznCv72Nc8gYlazwewn0ycAQgTmAFmNM76dqE4DEfNqGWqKF5fwraDwEgtIqbyOS7LO9xE6zhIZr3EFhbPrAf6aK1Lf8LmBAUUcPb76oj3BZHJlm8oH4A6YDEIv3NGW3V6KhCBhtsfc5ECIbrAvGzTDNuHnG4ReSieyiFhLuTK"
+    // query:"access_token="+access_token
+});
+
+socket.on('error', function(data){
+    console.log('errors', data);
+    if (data.status == 401 && data.error === "invalid_token") {
+        gh.tokenFetcher.removeCachedToken(access_token);
+        gh.tokenFetcher.getToken(true, function(err, token){
+            access_token = token;
+        });
+    }
+})
+
+socket.on('calls.ringing', function(data){
+    console.log('RINGING', data);
+    notify('ringing', data.calleridname, data.calleridnum);
+})
+
+socket.on('calls.ring', function(data){
+    console.log('HANGUP', data);
+    notify('ring', data.calleridname, data.calleridnum);
+})
+
+socket.on('calls.bridged', function(data){
+    console.log('BRIDGED', data);
+    notify('bridged', data.calleridname, data.calleridnum);
+})
+
+socket.on('calls.hangup', function(data){
+    console.log('HANGUP', data);
+    notify('hangup', data.calleridname, data.calleridnum);
+})
+
+function notify(type, msg, context) {
+    var title;
+    var opts = {
+        type: 'basic', 
+        iconUrl: 'icon128.png',
+        title: title || "", 
+        message: msg || "Unknown",
+        contextMessage: context || "",
+    }
+    switch (type){
+        case "ringing" :    
+            opts.title = "Appel entrant"; 
+            opts.iconUrl = 'incomming_call128.png'; 
+            break;
+        case "ring" :       
+            opts.title = "Appel sortant"; 
+            opts.iconUrl = 'icon128.png'; 
+            break;
+        case "hangup" :     
+            opts.title = "Raccroché"; 
+            opts.iconUrl = 'hangup_call128.png'; 
+            break;
+        case "bridged" :    
+            opts.title = "Communication établie"; 
+            opts.iconUrl = 'icon128.png'; 
+            break;
+        default :           
+            opts.title = type; 
+            break;
+    }
+    chrome.notifications.getAll(function(notifications){
+        if(notifications[type]){
+            chrome.notifications.clear(type, function(err){console.log('error', err)});
+        }
+    });
+    chrome.notifications.create(type, opts, function(n) {});
+}
