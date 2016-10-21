77b60d1cc786c23bfa3abb925af1f5a4855baf38
thivolle cedric <cedric@thivolle-cazat.fr>
2016-10-21 23:09:28 +0200

    :wip grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,67 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'voxity-chrome-extension/',
-        dest: 'dist/'
-      }
-    }
-  });
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        dirs: {
+            src:'voxity-chrome-extension',
+            dest:'dist'
+        },
+        concat: {
+            appChannels: {
+                src:[
+                    '<%= dirs.src %>/app/channels/channels.js',
+                    '<%= dirs.src %>/app/channels/services.js',
+                    '<%= dirs.src %>/app/channels/controllers.js',
+                ],
+                dest: '<%= dirs.dest %>/app/channels.js'
+            },
+            appContacts: {
+                src:[
+                    '<%= dirs.src %>/app/contacts/contacts.js',
+                    '<%= dirs.src %>/app/contacts/services.js',
+                    '<%= dirs.src %>/app/contacts/controllers.js',
+                    '<%= dirs.dest %>/app/contacts.tpl.min.js'
+                ],
+                dest: '<%= dirs.dest %>/app/contacts.js'
+            },
+        },
+        ngtemplates:{
+            'voxity.contacts':{
+                cwd: '<%= dirs.src %>/app',
+                src: '/views/contacts/**.html',
+                dest: '<%= dirs.dest %>/app/contacts.tpl.js',
+                options: {
+                    htmlmin:  {
+                        collapseBooleanAttributes:      true,
+                        collapseWhitespace:             true,
+                        removeAttributeQuotes:          true,
+                        removeComments:                 true,
+                        removeEmptyAttributes:          true,
+                        removeRedundantAttributes:      true,
+                        removeScriptTypeAttributes:     true,
+                        removeStyleLinkTypeAttributes:  true
+                    }
+                }
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+            },
+        },
+        uglify: {
+            appChannels:{
+                files: {
+                    '<%= dirs.dest %>/app/channels.min.js': ['<%= dirs.dest %>/app/channels.js'],
+                    '<%= dirs.dest %>/app/contacts.min.js': ['<%= dirs.dest %>/app/contacts.js'],
+                    '<%= dirs.dest %>/app/contacts.tpl.min.js': ['<%= dirs.dest %>/app/contacts.tpl.js']
+                }
+            }
+        }
+    });
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-angular-templates');
+
+    // Default task(s).
+    grunt.registerTask('default', ['ngtemplates', 'concat', 'uglify']);
 
 };
--- README.md
+++ README.md
@@ -19,10 +19,32 @@ Version non disponible
 4. Aller dans la gestion de vos extensions [chrome://extensions/]
 5. Activer le *Mode Développeur*
 6. cliquer sur *Charger l'extension non empaquetée...*
-7. Ajouter le dossier du projet
+7. Ajouter le dossier du projet *(/voxity-chrome-extension/)*
 8. modifier le fichier ``index.js``:83, la variable ```clientID```
 
 # Todo
- - [ ] Cache 
- - [ ] Configuration des services via des provider
+ - [X] Cache 
+ - [X] Configuration des services via des provider
  - [X] Gestion des droits admin
+ - [ ] Grunt Integration, minimify, concat, ... *(objectif 2 fichier index.min.html & app.min.js)*
+
+# install <small>(test build)</small>
+## Dépendance 
+
+* Google Chrome
+* bower 
+* être Utilisateur de la solution Voxity
+
+1. Clonner le projet 
+2. ce mettre à la racine du projet
+3. ```
+bower install
+npm install
+grunt
+```
+4. Aller dans la gestion de vos extensions [chrome://extensions/]
+5. Activer le *Mode Développeur*
+6. cliquer sur *Charger l'extension non empaquetée...*
+7. Ajouter le dossier du projet **(/dist/)**
+8. modifier le fichier ``index.js``:83, la variable ```clientID```
+9. ```grunt```
