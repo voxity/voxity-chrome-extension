fb5ffa5979117522c04b132cdba890f3a4034a0f
thivolle cedric <cedric@thivolle-cazat.fr>
2016-10-24 23:30:16 +0200

    :wip grunt build is ok, not tested finaly app

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,152 +1,174 @@
-var htmlminOpt = {
-    collapseBooleanAttributes:      true,
-    collapseWhitespace:             true,
-    removeAttributeQuotes:          true,
-    removeComments:                 true,
-    removeEmptyAttributes:          true,
-    removeRedundantAttributes:      true,
-    removeScriptTypeAttributes:     true,
-    removeStyleLinkTypeAttributes:  true
-};
+
 
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         dirs: {
+            srcAngApp: 'voxity-chrome-extension/app',
+            dstAngApp: 'dist/app',
             src:'voxity-chrome-extension',
             dest:'dist'
         },
+        vars :{
+            htmlminOpt: {
+                collapseBooleanAttributes:      true,
+                collapseWhitespace:             true,
+                removeAttributeQuotes:          true,
+                removeComments:                 true,
+                removeEmptyAttributes:          true,
+                removeRedundantAttributes:      true,
+                removeScriptTypeAttributes:     true,
+                removeStyleLinkTypeAttributes:  true
+            }
+        },
         concat: {
-            appCore:{
+            templates:{
                 src: [
-                    '<%= dirs.src %>/app/core/core.js',
-                    '<%= dirs.src %>/app/core/filters.js',
-                    '<%= dirs.src %>/app/core/services.js',
-                    '<%= dirs.src %>/app/core/controllers.js',
-                    '<%= dirs.dest %>/app/core.err.tpl.js',
-                    '<%= dirs.dest %>/app/core.settings.tpl.js',
+                    '<%= dirs.dstAngApp %>/index.min.js',
+                    '<%= ngtemplates.contacts.dest %>',
+                    '<%= ngtemplates.err.dest %>',
+                    '<%= ngtemplates.settings.dest %>',
+                    '<%= ngtemplates.devices.dest %>'
                 ],
-                dest: '<%= dirs.dest %>/app/core.js'
-            },
-            appUsers:{
-                src: [
-                    '<%= dirs.src %>/app/users/users.js',
-                    '<%= dirs.src %>/app/users/services.js',
-                ],
-                dest: '<%= dirs.dest %>/app/users.js'
-            },
-            appChannels: {
-                src:[
-                    '<%= dirs.src %>/app/channels/channels.js',
-                    '<%= dirs.src %>/app/channels/services.js',
-                    '<%= dirs.src %>/app/channels/controllers.js',
-                ],
-                dest: '<%= dirs.dest %>/app/channels.js'
-            },
-            appContacts: {
-                src:[
-                    '<%= dirs.src %>/app/contacts/contacts.js',
-                    '<%= dirs.src %>/app/contacts/services.js',
-                    '<%= dirs.src %>/app/contacts/controllers.js',
-                    '<%= dirs.dest %>/app/contacts.tpl.js'
-                ],
-                dest: '<%= dirs.dest %>/app/contacts.js'
-            },
-            appDevices: {
-                src:[
-                    '<%= dirs.src %>/app/devices/devices.js',
-                    '<%= dirs.src %>/app/devices/services.js',
-                    '<%= dirs.src %>/app/devices/controllers.js',
-                    '<%= dirs.dest %>/app/devices.tpl.js'
-                ],
-                dest: '<%= dirs.dest %>/app/devices.js'
-            },
-            fullApp:{
-                src:[
-                    '<%= dirs.dest %>/app/core.min.js',
-                    '<%= dirs.dest %>/app/users.min.js',
-                    '<%= dirs.dest %>/app/channels.min.js',
-                    '<%= dirs.dest %>/app/contacts.min.js',
-                    '<%= dirs.dest %>/app/devices.min.js',
-                    '<%= dirs.src %>/app/app.js',
-                    '<%= dirs.src %>/app/index.js'
-                ],
-                dest: '<%= dirs.dest %>/app/index.js'
+                dest:'<%= dirs.dstAngApp %>/index.min.js'
             }
         },
         ngtemplates:{
-            'voxity.contacts':{
-                cwd: '<%= dirs.src %>/app/views/contacts/',
+            'contacts':{
+                cwd: '<%= dirs.srcAngApp %>/views/contacts/',
                 src: '**.html',
-                dest: '<%= dirs.dest %>/app/contacts.tpl.js',
+                dest: '<%= dirs.dstAngApp %>/contacts.tpl.js',
                 options: {
+                    module: 'voxity.contacts',
                     prefix:'/views/contacts',
-                    htmlmin: htmlminOpt
+                    htmlmin: '<%= vars.htmlminOpt %>'
                 }
             },
             'err':{
-                cwd: '<%= dirs.src %>/app/views/err/',
+                cwd: '<%= dirs.srcAngApp %>/views/err/',
                 src: '**.html',
-                dest: '<%= dirs.dest %>/app/core.err.tpl.js',
+                dest: '<%= dirs.dstAngApp %>/core.err.tpl.js',
                 options: {
                     module: 'voxity.core',
                     prefix:'/views/err',
-                    htmlmin: htmlminOpt
+                    htmlmin: '<%= vars.htmlminOpt %>'
+
                 }
             },
             'settings': {
-                cwd: '<%= dirs.src %>/app/views/settings/',
+                cwd: '<%= dirs.srcAngApp %>/views/settings/',
                 src: '**.html',
-                dest: '<%= dirs.dest %>/app/core.settings.tpl.js',
+                dest: '<%= dirs.dstAngApp %>/core.settings.tpl.js',
                 options: {
                     module: 'voxity.core',
                     prefix:'/views/settings',
-                    htmlmin: htmlminOpt
+                    htmlmin: '<%= vars.htmlminOpt %>'
                 }
             },
-            'voxity.devices':{
-                cwd: '<%= dirs.src %>/app/views/devices/',
+            'devices':{
+                cwd: '<%= dirs.srcAngApp %>/views/devices/',
                 src: '**.html',
-                dest: '<%= dirs.dest %>/app/devices.tpl.js',
+                dest: '<%= dirs.dstAngApp %>/devices.tpl.js',
                 options: {
+                    module: 'voxity.devices',
                     prefix:'/views/devices',
-                    htmlmin: htmlminOpt
-                }
-            }
-
-        },
-        uglify: {
-            apps:{
-                files: {
-                    '<%= dirs.dest %>/app/core.min.js': ['<%= dirs.dest %>/app/core.js'],
-                    '<%= dirs.dest %>/app/users.min.js': ['<%= dirs.dest %>/app/users.js'],
-                    '<%= dirs.dest %>/app/channels.min.js': ['<%= dirs.dest %>/app/channels.js'],
-                    '<%= dirs.dest %>/app/contacts.min.js': ['<%= dirs.dest %>/app/contacts.js'],
-                    '<%= dirs.dest %>/app/devices.min.js': ['<%= dirs.dest %>/app/devices.js'],
-                }
-            },
-            app:{
-                files : {
-                    '<%= dirs.dest %>/app/index.min.js': ['<%= dirs.dest %>/app/index.js'],
+                    htmlmin: '<%= vars.htmlminOpt %>'
                 }
             }
         },
         clean: {
             dist: ['<%= dirs.dest %>/*'],
+            templates: ['<%= dirs.dstAngApp %>/*.tpl.js']
+        },
+        useminPrepare: {
+            html: '<%= dirs.srcAngApp %>/index.html',
+        },
+        usemin: {
+            html: ['<%= dirs.dstAngApp %>/index.html']
+        },
+        uglify: {
+            templates: {
+                files:[{
+                    dest: '<%= concat.templates.dest %>',
+                    src:[ '<%= concat.templates.dest %>',]
+                }]
+            }
+        },
+        cssmin: {
+            css: {
+                files:{
+                    '<%= dirs.dest %>/css/options.css': '<%= dirs.src %>/css/options.css'
+                }
+            }
+        },
+        copy: {
+            main:{
+                files: [
+                    {
+                        cwd:'<%= dirs.src %>/libs/assets/icons',
+                        src: ['*.*',],
+                        dest: '<%= dirs.dest %>/libs/assets/icons',
+                        expand: true
+                    },
+                    {
+                        cwd:'<%= dirs.src %>',
+                        src: ['*.*',],
+                        dest: '<%= dirs.dest %>',
+                        expand: true
+                    },
+                ]
+            },
+            angularApp: {
+                files: [
+                    {
+                        cwd:'<%= dirs.src %>/libs/assets/bootstrap/dist/',
+                        src: [
+                            '**',
+                            '!css/bootstrap-theme*',
+                            '!js/**'
+                        ],
+                        dest: '<%= dirs.dest %>/libs/assets/bootstrap/',
+                        expand: true
+                    },{
+                        cwd: '<%= dirs.src %>/libs/assets/font-awesome/',
+                        src: [
+                            'css/**',
+                            'fonts/**'
+                        ],
+                        dest: '<%= dirs.dest %>/libs/assets/font-awesome/',
+                        expand: true
+
+                    },
+                    {
+                        src: ['<%= dirs.srcAngApp %>/index.html'],
+                        dest: '<%= dirs.dstAngApp %>/index.html'
+                    }
+                ]
+            }
         }
     });
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-angular-templates');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-usemin');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     // Default task(s).
     grunt.registerTask('default', [
-        'clean:dist', 'ngtemplates',
-        'concat:appCore', 'concat:appUsers', 'concat:appChannels', 'concat:appContacts', 'concat:appDevices',
-        'uglify:apps',
-        'concat:fullApp', 'uglify:app']);
-
+        'clean:dist',
+        'copy',
+        'useminPrepare',
+        'concat:generated',
+        'uglify:generated',
+        'cssmin',
+        'usemin',
+        'ngtemplates',
+        'concat:templates',
+        'uglify:templates',
+        'clean:templates'
+    ]);
 };
